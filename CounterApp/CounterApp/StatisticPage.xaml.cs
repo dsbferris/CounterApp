27b44f7ce64819c80bd992c5c782ce969dc9a715
Ferris Schwanebeck <ferris.s@aol.de>
2020-03-22 18:26:51 +0100

    Created App. Adding statistics

--- CounterApp/CounterApp/StatisticPage.xaml.cs
+++ CounterApp/CounterApp/StatisticPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microcharts;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Entry = Microcharts.Entry;
 
 namespace CounterApp
 {
@@ -15,6 +18,30 @@ namespace CounterApp
 		public StatisticPage()
 		{
 			InitializeComponent();
+
+			var entries = new[]
+			{
+				new Entry(200)
+				{
+					Label = "January",
+					ValueLabel = "200",
+					Color = SKColor.Parse("#266489")
+				},
+				new Entry(400)
+				{
+					Label = "February",
+					ValueLabel = "400",
+					Color = SKColor.Parse("#68B9C0")
+				},
+				new Entry(-100)
+				{
+					Label = "March",
+					ValueLabel = "-100",
+					Color = SKColor.Parse("#90D585")
+				}
+			};
+			var chart = new BarChart() { Entries = entries };
+			chartView.Chart = chart;
 		}
 	}
 }
